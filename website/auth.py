e2332b863bf3830ad83223b9b88b99429a9c72b1
Toby Rachner <toby@rachner.com>
2024-03-02 14:49:01 -0600

    created databse and flask_login module

--- website/auth.py
+++ website/auth.py
@@ -1,15 +1,62 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, request, flash, url_for, redirect
+from flask_login import login_user, login_required, logout_user, current_user
+from werkzeug.security import generate_password_hash, check_password_hash
+from .models import User
+from . import db
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('login')
+@auth.route('login', methods=['GET', 'POST'])
 def login():
-    return "<h1>login</h1>"
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+
+        user = User.query.filter_by(email=email).first()
+        if user:
+            if user.password == password:
+                login_user(user, remember=True)
+                flash('Logged in successfully', category='success')
+                return redirect(url_for('views.home'))
+            else:
+                flash('Incorrect password', category='error')
+        else:
+            flash('User not found', category='error')
+    return render_template('login.html')
+
+@auth.route('sign-up', methods=['GET', 'POST'])
+def sign_up():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        username = request.form.get('username')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        user1 = User.query.filter_by(email=email).first()
+        user2 = User.query.filter_by(username=username).first()
+
+        if user1:
+            flash('Email already in use', category='error')
+        elif user2:
+            flash('Username already taken', category='error')
+        elif len(username) < 4:
+            flash('Username must be at least 4 characters', category='error')
+        elif password1 != password2:
+            flash('Passwords don\'t match', category='error')
+        elif len(password1) < 7:
+            flash('Password must be at least 7 characters', category='error')
+        else:
+            new_user = User(email=email, username=username, password=password1)
+            db.session.add(new_user)
+            db.session.commit()
+
+            flash('Account created successfully', category='success')
+            login_user(new_user, remember=True)
+            return redirect(url_for('views.home'))
+    return render_template('sign_up.html')
 
 @auth.route('logout')
+@login_required
 def logout():
-    return "<h1>logout</h1>"
-
-@auth.route('sign-up')
-def sign_up():
-    return "<h1>Sign up</h1>"
+    logout_user()
+    return redirect(url_for('auth.login'))
