2be973e43d136e64cf01782812acdfbd10866425
Toby Rachner <toby@rachner.com>
2024-03-05 19:19:23 -0600

    Added piece images and /pieces/<piece_name> route to view them

--- website/views.py
+++ website/views.py
@@ -1,11 +1,17 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, send_from_directory
 from flask_login import current_user
+import os
 
 views = Blueprint('views', __name__)
 
 @views.route('/')
 def home():
     return render_template('home.html', user=current_user)
+
+@views.route('/pieces/<piece>')
+def get_piece_image(piece):
+    path = os.path.join(os.path.dirname(os.getcwd()), 'online-chess', 'website', 'static', 'images', 'pieces')
+    return send_from_directory(path, piece.replace('-', '_') + '.png')
 
 @views.route('/room/<id>')
 def game_room(id):
